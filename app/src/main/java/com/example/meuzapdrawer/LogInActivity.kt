f950155043312125701d4a07e091da0ff5ce19a0
DouglasANS <douglasans@hotmail.com>
2021-06-29 17:51:39 -0300

    First Commit

---
+++ app/src/main/java/com/example/meuzapdrawer/LogInActivity.kt
@@ -0,0 +1,62 @@
+package com.example.meuzapdrawer
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class LogInActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build() ,
+            AuthUI.IdpConfig.GoogleBuilder().build()
+
+//provider
+        )
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN);
+
+        }
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                //val user = FirebaseAuth.getInstance().currentUser
+                // ...
+                val intent: Intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, response?.error?.message, Toast.LENGTH_LONG).show()
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/meuzapdrawer/Splash.kt
@@ -0,0 +1,24 @@
+package com.example.meuzapdrawer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        val intent:Intent = if (user == null){
+            Intent(this, LogInActivity::class.java)
+        }else{
+            Intent(this, MainActivity::class.java)
+        }
+        startActivity(intent)
+        finish()
+    }
+
+
+}
