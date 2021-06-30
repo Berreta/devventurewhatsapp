4362e402d4225793d6c18b6679f6f92d77379df2
DouglasANS <douglasans@hotmail.com>
2021-06-30 18:34:25 -0300

    fragment home

--- app/src/main/java/com/example/meuzapdrawer/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/meuzapdrawer/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.meuzapdrawer.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.meuzapdrawer.LogInActivity
+import com.example.meuzapdrawer.MainActivity
 import com.example.meuzapdrawer.R
 import com.example.meuzapdrawer.databinding.FragmentHomeBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
@@ -35,8 +40,11 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
